fd0ce69b49fb69a84db3a3d2c0288e2dd34070d2
Adriana Lpz <adrianalsz@hotmail.com>
2021-10-26 12:50:59 -0500

    install

---
+++ figuras.py
@@ -0,0 +1,25 @@
+import numpy
+from turtle import *
+
+
+speed(0)
+
+color("red")
+fillcolor("green")
+
+s = 200 #tamaño cuadrado
+
+#cuadrado
+for i in range(4):
+    fd(s)
+    left(90)
+
+
+#triángulo
+theta = 60
+r = math.sqrt((s**2)+((s/2)**2))
+left(theta)
+fd(r)
+
+
+done()
