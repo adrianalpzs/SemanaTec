12a289a4e7abd56b3e1d5118b444697c306106d2
Adriana Lpz <adrianalsz@hotmail.com>
2021-10-26 13:36:34 -0500

    foodAct2

---
+++ act2.py
@@ -0,0 +1,71 @@
+from turtle import *
+from random import randrange
+from freegames import square, vector
+import random
+
+colors=['blue', 'green', 'purple','yellow','orange']
+
+bodyc=random.choice(colors)
+
+foodc=random.choice(colors)
+
+if foodc==bodyc:
+    foodc=random.choice(colors)
+
+food = vector(0, 0)
+snake = [vector(10, 0)]
+aim = vector(0, -10)
+
+def mFood():
+    food.x = randrange(-15, 15) * 10
+    food.y = randrange(-15, 15) * 10
+    ontimer(mFood, 4500)
+
+def change(x, y):
+    "Change snake direction."
+    aim.x = x
+    aim.y = y
+
+def inside(head):
+    "Return True if head inside boundaries."
+    return -200 < head.x < 190 and -200 < head.y < 190
+
+def move():
+    "Move snake forward one segment."
+    head = snake[-1].copy()
+    head.move(aim)
+
+    if not inside(head) or head in snake:
+        square(head.x, head.y, 9, 'red')
+        update()
+        return
+
+    snake.append(head)
+
+    if head == food:
+        print('Snake:', len(snake))
+        food.x = randrange(-15, 15) * 10
+        food.y = randrange(-15, 15) * 10
+    else:
+        snake.pop(0)
+
+    clear()
+
+    for body in snake:
+        square(body.x, body.y, 9, bodyc)
+
+    square(food.x, food.y, 9, foodc)
+    update()
+    ontimer(move, 100)
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+listen()
+onkey(lambda: change(10, 0), 'Right')
+onkey(lambda: change(-10, 0), 'Left')
+onkey(lambda: change(0, 10), 'Up')
+onkey(lambda: change(0, -10), 'Down')
+mFood()
+move()
+done()
